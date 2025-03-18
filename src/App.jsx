614b6399148d512b00bdc789c305da2f8dfe3143
kavitasuthar123 <115469001+kavitasuthar123@users.noreply.github.com>
2025-03-18 11:12:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-// import { useState } from 'react'
-
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div className='grid-container'>
-
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import { useState } from 'react'
 import './App.css'
